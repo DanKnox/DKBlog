44e878d270e1dad4a19a5d474aa233161d566464
Dan Knox <dknox@threedotloft.com>
2011-12-27 11:12:26 -0800

    omniauth still not picking up user hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   
   class << self
     def find_for_github_oauth(access_token, signed_in_resource=nil)
-      data = access_token.extra.raw_info
+      data = access_token.extra.user_hash
       if user = User.where(:email => data.email).first
         user
       else # Create a user with a stub password. 
