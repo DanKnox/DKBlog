ed29a81155648062145847812e21618693490383
Dan Knox <dknox@threedotloft.com>
2011-12-26 19:54:44 -0800

    ruby 1.8.7 fixes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Danknox::Application.config.session_store :cookie_store, key: '_danknox_session'
+Danknox::Application.config.session_store :cookie_store, :key => '_danknox_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
